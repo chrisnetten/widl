f60efca26403c613c2ec2921538335874dc5d186
chris netten <200230280@MyGeorgian.ca>
2016-02-20 20:03:12 -0500

    animation testing

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,13 @@
 $(window).load(function() {
 	$(".siteLoadUp").fadeOut("slow");
 });
+
+$(document).ready(function(){
+   $('#demo1').animo( { animation: 'bounceInLeft', duration: 3 } );
+	$('#demo2').animo( { animation: 'bounceInRight', duration: 3 } );
+
+
+$('#tada').animo( { animation: 'tada' } );
+$('#tada1').animo( { animation: 'tada' } );
+$('#tada2').animo( { animation: 'tada' } );
+});
