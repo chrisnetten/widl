b48a5ae8a3a1bd4a3dd4a03b27f6710a4f04ff6f
chris netten <200230280@MyGeorgian.ca>
2016-02-20 21:14:09 -0500

    removal of a couple animations

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,28 +2,40 @@ $(window).load(function() {
 	$(".siteLoadUp").fadeOut("slow");
 });
 
+/* service animations */
 $(document).ready(function(){
-       $("#offer-p").hide();
-       $(".small-box").hide();
-    $('#h1-offer').animo( {animation: 'bounceInLeft', duration: 2 }, function() {
-           $('#offer-p').fadeToggle(500);
-           $('.small-box').fadeToggle(500);
-    });
+      
+
    $('#demo1').animo( { animation: 'bounceInLeft', duration: 3 } );
 	$('#demo2').animo( { animation: 'bounceInRight', duration: 3 } );
 
+	$('#service-intro').animo( { animation: 'fadeInUp', duration: 1 } );
 
-$('#tada').animo( { animation: 'tada' } );
-$('#tada1').animo( { animation: 'tada' } );
-$('#tada2').animo( { animation: 'tada' } );
-});
+    });
 
+
+/* Index animations */
 $(document).ready(function() {
+   
+    $("#offer-p").hide();
+       $(".small-box").hide();
+    $('#h1-offer').animo( {animation: 'bounceInLeft', duration: 2 }, function() {
+           $('#offer-p').fadeToggle(500);
+           $('.small-box').fadeToggle(500); 
+    });
    
    $('#button').animo("rotate", { degrees:360 }, function() {
 	$('#button1').animo("rotate", { degrees:360 }, function() {
 		$('#button2').animo("rotate", { degrees:360 });
+        
+        
 		});
 	});
-});
     
+    $('#index-img').animo({animation: "fadeInLeft", duration: 1});
+			$('#index-img1').animo({animation: "fadeInUp", duration: 1});
+				$('#index-img2').animo({animation: "fadeInRight", duration: 1});
+			});
+	
+/* About Animations */
+
