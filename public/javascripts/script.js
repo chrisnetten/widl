4c29e6d5b67c57cecbca65c0ed924b40c49a0b39
chris netten <200230280@MyGeorgian.ca>
2016-02-20 20:06:09 -0500

    button spin

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -11,3 +11,13 @@ $('#tada').animo( { animation: 'tada' } );
 $('#tada1').animo( { animation: 'tada' } );
 $('#tada2').animo( { animation: 'tada' } );
 });
+
+$(document).ready(function() {
+   
+   $('#button').animo("rotate", { degrees:360 }, function() {
+	$('#button1').animo("rotate", { degrees:360 }, function() {
+		$('#button2').animo("rotate", { degrees:360 });
+		});
+	});
+});
+    
