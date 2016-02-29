66f751843803ad0b942a7d0cdca3f487881fb0e0
chris netten <200230280@MyGeorgian.ca>
2016-02-29 17:49:17 -0500

    reorganize

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,19 +2,7 @@ $(window).load(function() {
 	$(".siteLoadUp").fadeOut("slow");
 });
 
-/* service animations */
-$(document).ready(function(){
-      
 
-   $('#demo1').animo( { animation: 'bounceInLeft', duration: 3 } );
-	$('#demo2').animo( { animation: 'bounceInRight', duration: 3 } );
-
-	$('#service-intro').animo( { animation: 'fadeInUp', duration: 1 } );
-    
-    	$('#test').animo( { animation: 'bounceInRight', duration: 3 } );
-            	$('#test1').animo( { animation: 'bounceInLeft', duration: 3 } );
-                
-});
                 
  $(document).ready(function(){
      new WOW().init();
